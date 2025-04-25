3f64b9e290a4b2ae2e170f31daec672f3bcb1a2a
caiokirst <caiokirst@gmail.com>
2025-04-25 19:57:20 -0300

    Html e CSS prontos

--- react/Cadastro_usuario/src/pages/Home/index.jsx
+++ react/Cadastro_usuario/src/pages/Home/index.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
-  const [count, setCount] = useState(0)
+
+  const users = [
+    {
+      id: 1,
+      name: 'Lucas',
+      age: 22,
+      email: 'lucas@gmail.com'
+    }
+    ,
+    {
+      id: 2,
+      name: 'Ana',
+      age: 25,
+      email: 'ana@gmail.com'
+    }
+    ,
+    {
+      id: 3,
+      name: 'João',
+      age: 30,
+      email: 'joão@gmail.com',
+    }]
 
   return (
-  
-      <div>
-         <h1>Cadastro de Usuário</h1>
-      </div>
-     
+
+    <div className='container'>
+      <form className='form'>
+        <h1>Cadastro de Usuário</h1>
+        <input name='nome' type="text" placeholder='Nome' />
+        <input name='idade' type="number" placeholder='Idade' />
+        <input name='email' type="email" placeholder='Email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span> </p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
+
+
+    </div>
+
   )
 }
 
