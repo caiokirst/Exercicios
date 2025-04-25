d68e9f66a6c8cd1ec53391c24f1db4c16f7318b5
caiokirst <caiokirst@gmail.com>
2025-04-25 16:00:30 -0300

    upload

---
+++ react/Cadastro_usuario/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div>
+         <h1>Cadastro de Usuário</h1>
+      </div>
+     
+  )
+}
+
+export default Home
